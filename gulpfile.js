55158632233d44315a5b430b7f77ccf4d669422e
unknown <zhangkang103@gmail.com>
2017-05-19 23:54:21 +0800

    add gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+
+var sourcemaps = require('gulp-sourcemaps');
+
+var less = require('gulp-less');
+var path = require('path');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
+
+
+var babel = require('gulp-babel');
+
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+// 静态服务器
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('src/*.less', ['less']);
+    gulp.watch('src/*.js', ['js']);
+    gulp.watch('index.html', ['html']);
+});
+
+
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(reload({ stream: true }))
+})
+
+gulp.task('less', function () {
+    return gulp.src('src/*.less')
+        .pipe(sourcemaps.init())
+        .pipe(less({
+            plugins: [autoprefix]
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({ stream: true }))
+})
+
+gulp.task('js', function () {
+    console.log('js-----doing---------')
+    return gulp.src('src/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({ stream: true }))
+})
+
+gulp.task('default', ['browser-sync'])
