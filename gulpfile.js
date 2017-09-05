c90f26d9537ae0161c41e8cc0ce36f65ff7fa8a6
RalfZhang <zhangkang103@gmail.com>
2017-09-05 17:59:32 +0800

    [功能] 添加 path 配置文件

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,19 @@ var reload = browserSync.reload;
 
 var autoprefixConfig= ['last 2 version', 'ie 8', 'ie 9'];
 var path = './'
+var pathFiles= {
+    html: path+'*.html',
+    less: path+'src/*.less',
+    scss: path+'src/*.scss',
+    js: path+'src/*.js',
+    start: path+'index.html'
+}
+var pathDistFiles = {
+    html: path+'dist',
+    less: path+'dist',
+    scss: path+'dist',
+    js: path+'dist',
+}
 
 // 静态服务器
 gulp.task('browser-sync', function () {
@@ -25,22 +38,22 @@ gulp.task('browser-sync', function () {
         server: {
             baseDir: "./"
         },
-        startPath: path+'index.html'
+        startPath: pathFiles.start
     });
-    gulp.watch('src/*.less', ['less']);
-    gulp.watch('src/*.scss', ['scss']);
-    gulp.watch('src/*.js', ['js']);
-    gulp.watch('index.html', ['html']);
+    gulp.watch(pathFiles.less, ['less']);
+    gulp.watch(pathFiles.scss, ['scss']);
+    gulp.watch(pathFiles.js, ['js']);
+    gulp.watch(pathFiles.html, ['html']);
 })
 
 
 gulp.task('html', function () {
-    return gulp.src(path+'index.html')
+    return gulp.src(pathFiles.html)
         .pipe(reload({ stream: true }))
 })
 
 gulp.task('less', function () {
-    return gulp.src(path+'src/*.less')
+    return gulp.src(pathFiles.less)
         .pipe(sourcemaps.init())
         .pipe(less({
             plugins: [ new LessAutoprefix({ 
@@ -48,32 +61,32 @@ gulp.task('less', function () {
             })]
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(pathDistFiles.less))
         .pipe(reload({ stream: true }))
 })
 
 gulp.task('scss', function () {
-    return gulp.src(path+'src/*.scss')
+    return gulp.src(pathFiles.scss)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: autoprefixConfig
         }))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(pathDistFiles.scss))
         .pipe(reload({ stream: true }))
 })
 
 
 gulp.task('js', function () {
     console.log('js-----doing---------')
-    return gulp.src(path+'src/*.js')
+    return gulp.src(pathFiles.js)
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(pathDistFiles.js))
         .pipe(reload({ stream: true }))
 })
 
