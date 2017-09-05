74d7f88a0848058adea8080ba3b9fce12bdc4efc
RalfZhang <zhangkang103@gmail.com>
2017-09-05 15:01:38 +0800

    更新 sass autoprefixer \n配置 更新自动打开页面

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var sourcemaps = require('gulp-sourcemaps');
 
 var less = require('gulp-less');
 var path = require('path');
+var autoprefixer = require('gulp-autoprefixer');
 var LessAutoprefix = require('less-plugin-autoprefix');
-var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 var sass = require('gulp-sass');
 
@@ -16,12 +16,16 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 
+var autoprefixConfig= ['last 2 version', 'ie 8', 'ie 9'];
+var path = './'
+
 // 静态服务器
 gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./"
-        }
+        },
+        startPath: path+'index.html'
     });
     gulp.watch('src/*.less', ['less']);
     gulp.watch('src/*.scss', ['scss']);
@@ -31,15 +35,17 @@ gulp.task('browser-sync', function () {
 
 
 gulp.task('html', function () {
-    return gulp.src('index.html')
+    return gulp.src(path+'index.html')
         .pipe(reload({ stream: true }))
 })
 
 gulp.task('less', function () {
-    return gulp.src('src/*.less')
+    return gulp.src(path+'src/*.less')
         .pipe(sourcemaps.init())
         .pipe(less({
-            plugins: [autoprefix]
+            plugins: [ new LessAutoprefix({ 
+                browsers: autoprefixConfig 
+            })]
         }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'))
@@ -47,9 +53,12 @@ gulp.task('less', function () {
 })
 
 gulp.task('scss', function () {
-    return gulp.src('src/*.scss')
+    return gulp.src(path+'src/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: autoprefixConfig
+        }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'))
         .pipe(reload({ stream: true }))
@@ -58,7 +67,7 @@ gulp.task('scss', function () {
 
 gulp.task('js', function () {
     console.log('js-----doing---------')
-    return gulp.src('src/*.js')
+    return gulp.src(path+'src/*.js')
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['es2015']
